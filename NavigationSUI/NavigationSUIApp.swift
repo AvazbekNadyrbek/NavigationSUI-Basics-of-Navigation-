524d9620b0f535d52f58001a0854033090018cb5
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-11-09 18:40:06 +0100

    Initial Commit

---
+++ NavigationSUI/NavigationSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSUIApp.swift
+//  NavigationSUI
+//
+//  Created by Авазбек Надырбек уулу on 09.11.25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
